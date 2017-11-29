1e7d51aea8b793c1cfc36abe06897c0de20b6c01
Nick Endle <nick.endle@gmail.com>
2017-11-29 10:36:24 -0600

    move scrolltrigger js into store.js

--- source/javascripts/store.js
+++ source/javascripts/store.js
@@ -27,3 +27,19 @@ function parallax(){
   var scrolled = $(window).scrollTop();
   $('.accent-background').css('top',-(scrolled*0.20)+'px');
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+	var trigger = new ScrollTrigger({
+	  toggle: {
+	    visible: 'visible',
+	    hidden: 'invisible'
+	  },
+	  offset: {
+	    x: 0,
+	    y: 60
+	  },
+	  addHeight: false,
+	  once: true
+	}, document.body, window);
+});
+
