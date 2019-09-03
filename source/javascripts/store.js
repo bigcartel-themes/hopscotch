25692d0bdb6bb3be32a5cdf61ad66cbaeefda4b9
Nick Endle <nick.endle@gmail.com>
2019-09-03 13:51:45 -0500

    Add product option group JS

--- source/javascripts/store.js
+++ source/javascripts/store.js
@@ -1,6 +1,6 @@
 var inPreview = (/\/admin\/design/.test(top.location.pathname));
 
-$('.dropdown-navigation-title').click(function(e) { 
+$('.dropdown-navigation-title').click(function(e) {
   $('.dropdown-navigation-list').toggleClass('visible');
 });
 
@@ -11,14 +11,6 @@ $('.option-quantity').blur(function(e) {
 $('.cart-item-remove').click(function(e) {
   $(this).closest('li').find('input.option-quantity').val(0).closest('form').submit();
   return false;
-});
-
-$('.product-option-list li').not('.disabled').click(function() { 
-  var option_id = $(this).data("option-id");
-  if (option_id > 0) { 
-    $('#option').val(option_id);
-    $('.product-form').submit();
-  }
 });
 
 $(window).scroll(function(e){
@@ -46,9 +38,155 @@ if (!inPreview) {
   	}, document.body, window);
   });
 }
-else { 
+else {
   $('div[data-scroll]').each(function() {
     $(this).addClass('visible');
   });
 }
+var isGreaterThanZero = function(currentValue) {
+  return currentValue > 0;
+}
 
+function arrayContainsArray(superset, subset) {
+  if (0 === subset.length) {
+    return false;
+  }
+  return subset.every(function (value) {
+    return (superset.indexOf(value) >= 0);
+  });
+}
+
+function unique(item, index, array) {
+  return array.indexOf(item) == index;
+}
+
+function cartesianProduct(a) {
+  var i, j, l, m, a1, o = [];
+  if (!a || a.length == 0) return a;
+  a1 = a.splice(0, 1)[0];
+  a = cartesianProduct(a);
+  for (i = 0, l = a1.length; i < l; i++) {
+    if (a && a.length) for (j = 0, m = a.length; j < m; j++)
+      o.push([a1[i]].concat(a[j]));
+    else
+      o.push([a1[i]]);
+  }
+  return o;
+}
+
+Array.prototype.equals = function (array) {
+  if (!array)
+    return false;
+  if (this.length != array.length)
+    return false;
+  for (var i = 0, l=this.length; i < l; i++) {
+    if (this[i] instanceof Array && array[i] instanceof Array) {
+      if (!this[i].equals(array[i]))
+        return false;
+    }
+    else if (this[i] != array[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// From https://github.com/kevlatus/polyfill-array-includes/blob/master/array-includes.js
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value: function (searchElement, fromIndex) {
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+      var o = Object(this);
+      var len = o.length >>> 0;
+      if (len === 0) {
+        return false;
+      }
+      var n = fromIndex | 0;
+      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+      function sameValueZero(x, y) {
+        return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+      }
+      while (k < len) {
+        if (sameValueZero(o[k], searchElement)) {
+          return true;
+        }
+        k++;
+      }
+      return false;
+    }
+  });
+}
+
+Array.prototype.count = function(filterMethod) {
+  return this.reduce((count, item) => filterMethod(item)? count + 1 : count, 0);
+}
+if ($('.product_option_select').length) {
+  disableAddButton();
+}
+$('.product_option_select').on('change',function() {
+  var option_price = $(this).find("option:selected").attr("data-price");
+  enableAddButton(option_price);
+});
+function enableAddButton(updated_price) {
+  var addButton = $('.add-to-cart-button');
+  var addButtonTitle = addButton.attr('data-add-title');
+  addButton.attr("disabled",false);
+  if (updated_price) {
+    priceTitle = ' - ' + Format.money(updated_price, true, true);
+  }
+  else {
+    priceTitle = '';
+  }
+  addButton.html(addButtonTitle + priceTitle);
+}
+
+function disableAddButton(type) {
+  var addButton = $('.add-to-cart-button');
+  var addButtonTitle = addButton.attr('data-add-title');
+  if (type == "sold-out") {
+    var addButtonTitle = addButton.attr('data-sold-title');
+  }
+  if (!addButton.is(":disabled")) {
+    addButton.attr("disabled","disabled");
+  }
+  addButton.html(addButtonTitle);
+}
+
+function enableSelectOption(select_option) {
+  select_option.removeAttr("disabled");
+  select_option.text(select_option.attr("data-name"));
+  select_option.removeAttr("disabled-type");
+  if ((select_option.parent().is('span'))) {
+    select_option.unwrap();
+  }
+}
+function disableSelectOption(select_option, type) {
+  if (type === "sold-out") {
+    disabled_text = select_option.parent().attr("data-sold-text");
+    disabled_type = "sold-out";
+    if (show_sold_out_product_options === 'false') {
+      hide_option = true;
+    }
+    else {
+      hide_option = false;
+    }
+  }
+  if (type === "unavailable") {
+    disabled_text = select_option.parent().attr("data-unavailable-text");
+    disabled_type = "unavailable";
+    hide_option = true;
+  }
+  if (select_option.val() > 0) {
+    var name = select_option.attr("data-name");
+    select_option.attr("disabled",true);
+    select_option.text(name + ' ' + disabled_text);
+    select_option.attr("disabled-type",disabled_type);
+    if (hide_option === true) {
+      if (!(select_option.parent().is('span'))) {
+        select_option.wrap('<span>');
+      }
+    }
+  }
+}
