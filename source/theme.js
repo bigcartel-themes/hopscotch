17774ffd3d4b35c259629743081870ba217f39dd
Nick Endle <nick@bigcartel.com>
2024-03-14 17:41:31 -0500

    Add new image gallery library and clean up misc. code

--- source/theme.js
+++ source/theme.js
@@ -1,3 +1,6 @@
 //= require_directory ./javascripts/vendor
+//= require javascripts/functions
 //= require javascripts/store
+//= require javascripts/product
+//= require javascripts/product-carousel
 //= require javascripts/product-option-groups
